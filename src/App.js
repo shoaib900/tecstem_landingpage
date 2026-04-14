ccf406059404bf4291141ce09f64943a0f2250ef
shoaib900 <rshoaib376@gmail.com>
2026-04-14 10:22:23 +0500

    update data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import Home from './Routes/Home'
 
 const App = () => {
   return (
+    <BrowserRouter>
     <div>
-      <Home />
-      
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   )
 }
 
